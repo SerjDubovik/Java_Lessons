e47c8d9e81c2d2df562c8253e1bc7ff1d4b8ffa2
Serj <mail@pufr.ru>
2023-06-20 21:47:54 +0300

    добавил урок 4

--- lessons_3_LinkedList_How/src/main/java/ru/pufr/Main.java
+++ lessons_3_LinkedList_How/src/main/java/ru/pufr/Main.java
@@ -3,5 +3,18 @@ package ru.pufr;
 public class Main {
     public static void main(String[] args) {
 
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(3);
+        myLinkedList.add(4);
+
+        System.out.println(myLinkedList);
+        System.out.println(myLinkedList.get(1));
+
+        myLinkedList.remove(2);
+        System.out.println(myLinkedList);
+
     }
 }
